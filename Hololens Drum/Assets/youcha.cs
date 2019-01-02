f70804145f46f25f710ccbd3c1f21812f09e933d
MatKal <ga.lty-1997@hotmail.com>
2019-01-02 11:44:13 +0100

    Init

---
+++ Hololens Drum/Assets/youcha.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class youcha : MonoBehaviour {
+    AudioSource ding;
+
+	// Use this for initialization
+	void Start () {
+        ding = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider cha)
+    {
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ding.Play(delay: 0);
+
+    }
+}
